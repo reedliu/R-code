93c014f222fa82596431635f80fda606dc65bcd1
reedliu <jieandze1314@gmail.com>
2020-02-25 13:40:46 +0800

    add

--- 多张图组合修改字体并导出PPT/多张图组合修改字体并导出PPT.R
+++ 多张图组合修改字体并导出PPT/多张图组合修改字体并导出PPT.R
@@ -22,31 +22,33 @@ pp
 
 
 ## 之后分别调整theme字体（设成6号大小），再组合
-setting=element_text(size = 10,color="black",family="Courier")
+# 先设置好setting，后面的可以随便做了
+# 设置element_blank()可以不显示（比如Y轴title）
+setting=element_text(size = 6,color="black",family="Arial")
 
 p1x=p1+theme(axis.title.x = setting,
-             axis.title.y = setting)+
+             axis.title.y = element_blank())+
     theme(axis.text.x = setting,
           axis.text.y = setting,
           legend.text = setting,
           legend.title = setting)
 
 p2x=p2+theme(axis.title.x = setting,
-             axis.title.y = setting)+
+             axis.title.y = element_blank())+
     theme(axis.text.x = setting,
           axis.text.y = setting,
           legend.text = setting,
           legend.title = setting)
 
 p3x=p3+theme(axis.title.x = setting,
-             axis.title.y = setting)+
+             axis.title.y = element_blank())+
     theme(axis.text.x = setting,
           axis.text.y = setting,
           legend.text = setting,
           legend.title = setting)
 
 p4x=p4+theme(axis.title.x = setting,
-             axis.title.y = setting)+
+             axis.title.y = element_blank())+
     theme(axis.text.x = setting,
           axis.text.y = setting,
           legend.text = setting,
@@ -55,8 +57,8 @@ p4x=p4+theme(axis.title.x = setting,
 # 排两列，加a-d标记，导出为PPT
 pp2x = p1x + p2x + p3x + p4x + plot_layout(ncol = 2) + 
     plot_annotation(tag_levels = 'a')
-pp2x
+# pp2x
 # 这个长宽直接影响到结果的图的比例
-graph2ppt(pp2x,"pp2x.pptx",width = 10, height = 8)
+graph2ppt(pp2x,"pp2x.pptx",width = 10, height = 8, orient="portrait")
 
 
